5c5446fc20dcaf79df5a9c8f95379f7520efee61
Smrutiranjan Jena <smrutiranjanjena411@gmail.com>
2025-03-12 10:34:30 +0530

    Updated face images and removed unused files

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Install system dependencies
-apt-get update && apt-get install -y cmake g++ libopenblas-dev liblapack-dev libx11-dev libgtk2.0-dev
-
-# Install Python dependencies
-pip install -r requirements.txt
